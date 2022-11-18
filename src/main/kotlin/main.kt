9c0643412448026c0d33f06f39f0c5cc849ff503
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-17 21:36:34 -0300

    Testando o for loop e whilel

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,38 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val titular = "Alexandre"
-    println("titular $titular")
 
-    val numeroConta = 4040
-    var saldoConta = 0.0
-//    saldoConta += 200
-//    saldoConta -= 1000
+    /*for (i in 5 downTo 1 step 2) {
+        val titular = "Alexandre $i"
+        println("titular $titular")
 
-    println("numero da conta $numeroConta")
-    println("saldo da conta $saldoConta")
+        val numeroConta = 4040 + i
+        var saldoConta = 10.0 + i
 
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldoConta")
+        println()
+    }*/
+
+    var i = 1
+    while (i <= 5) {
+        val titular = "Alexandre $i"
+        println("titular $titular")
+
+        val numeroConta = 4040 + i
+        var saldoConta = 10.0 + i
+
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldoConta")
+        println()
+        i++
+    }
+
+//    testaCondicoes(saldoConta)
+
+}
+
+private fun testaCondicoes(saldoConta: Double) {
     if (saldoConta > 0.0) {
         println("conta eh positiva")
     } else if (saldoConta == 0.0) {
@@ -25,5 +46,4 @@ fun main() {
         saldoConta == 0.0 -> println("conta eh neutra")
         saldoConta < 0.0 -> println("conta eh negativa")
     }
-
 }
