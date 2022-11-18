a56ccb9c1d76be6e358b38f9d0a4e3012175e2a5
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-17 21:11:00 -0300

    Primeiros passos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,4 +3,11 @@ fun main() {
 
     val titular = "Alexandre"
     println("titular $titular")
+
+    val numeroConta = 4040
+    var saldoConta = 0.0
+    saldoConta += 200
+
+    println("numero da conta $numeroConta")
+    println("saldo da conta $saldoConta")
 }
