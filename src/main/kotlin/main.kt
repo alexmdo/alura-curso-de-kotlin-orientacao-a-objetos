50ce6b95d7bd19cba72a8668bde5192267b6d032
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-17 21:22:32 -0300

    Testando if e when

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,8 +6,24 @@ fun main() {
 
     val numeroConta = 4040
     var saldoConta = 0.0
-    saldoConta += 200
+//    saldoConta += 200
+//    saldoConta -= 1000
 
     println("numero da conta $numeroConta")
     println("saldo da conta $saldoConta")
+
+    if (saldoConta > 0.0) {
+        println("conta eh positiva")
+    } else if (saldoConta == 0.0) {
+        println("conta eh neutra")
+    } else {
+        println("conta eh negativa")
+    }
+
+    when {
+        saldoConta > 0.0 -> println("conta eh positiva")
+        saldoConta == 0.0 -> println("conta eh neutra")
+        saldoConta < 0.0 -> println("conta eh negativa")
+    }
+
 }
